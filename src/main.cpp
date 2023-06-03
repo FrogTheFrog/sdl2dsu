87da312bc7b2a4453652cf9591f53927b1761342
FrogTheFrog <warliukz@gmail.com>
2023-06-03 20:57:33 +0300

    Add init delay

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 // system includes
 #include <boost/asio/co_spawn.hpp>
 #include <boost/asio/signal_set.hpp>
+#include <boost/asio/steady_timer.hpp>
 #include <boost/log/expressions.hpp>
 #include <boost/log/trivial.hpp>
 #include <boost/program_options.hpp>
@@ -14,6 +15,10 @@
 
 namespace
 {
+using namespace std::chrono_literals;
+
+//--------------------------------------------------------------------------------------------------
+
 void exceptionHandler(std::exception_ptr exception)
 {
     if (exception)
@@ -24,8 +29,8 @@ void exceptionHandler(std::exception_ptr exception)
 
 //--------------------------------------------------------------------------------------------------
 
-bool parseProgramArgs(int argc, const char* const* const argv, std::uint16_t& port, std::regex& controller_name_filter,
-                      std::string& mapping_file, bool& sensor_auto_toggle)
+bool parseProgramArgs(int argc, const char* const* const argv, int& init_delay, std::uint16_t& port,
+                      std::regex& controller_name_filter, std::string& mapping_file, bool& sensor_auto_toggle)
 {
     try
     {
@@ -38,6 +43,8 @@ bool parseProgramArgs(int argc, const char* const* const argv, std::uint16_t& po
         po::options_description desc("Available options");
         desc.add_options()                                                                                            //
             ("help", "print this help message")                                                                       //
+            ("delay", po::value<int>(&init_delay)->default_value(0),                                                  //
+             "time to wait in seconds before the app starts initialising SDL and opening server port")                //
             ("port", po::value<std::uint16_t>(&port)->required(), "port to use for DSU server")                       //
             ("filter", po::value<std::string>(&filter)->default_value(".*"),                                          //
              "regular expression (case-insensitive) to filter the controller names that we want to observe")          //
@@ -83,6 +90,53 @@ bool parseProgramArgs(int argc, const char* const* const argv, std::uint16_t& po
 
     return true;
 }
+
+//--------------------------------------------------------------------------------------------------
+
+bool interuptable_sleep(int init_delay)
+{
+    // Sleep if needed
+    if (init_delay > 0)
+    {
+        BOOST_LOG_TRIVIAL(info) << "Delaying initialization for " << init_delay << " second(s).";
+
+        // Using corountines here for sleeping so that it can be interupted
+        constexpr int           no_concurrency{1};
+        boost::asio::io_context io_context{no_concurrency};
+        boost::asio::signal_set signals(io_context, SIGINT, SIGTERM);
+
+        bool interrupted{false};
+        signals.async_wait(
+            [&io_context, &interrupted](auto, auto)
+            {
+                interrupted = true;
+                io_context.stop();
+            });
+
+        bool finished_sleeping{false};
+        boost::asio::co_spawn(
+            io_context,
+            [init_delay, &finished_sleeping, &io_context]() -> boost::asio::awaitable<void>
+            {
+                boost::asio::steady_timer timer(co_await boost::asio::this_coro::executor);
+                timer.expires_after(init_delay * 1s);
+
+                co_await timer.async_wait(boost::asio::use_awaitable);
+
+                finished_sleeping = true;
+                io_context.stop();
+            },
+            exceptionHandler);
+
+        io_context.run();
+        if (interrupted || !finished_sleeping)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
 }  // namespace
 
 //--------------------------------------------------------------------------------------------------
@@ -91,20 +145,28 @@ int main(int argc, char** argv)
 {
     try
     {
+        int           init_delay;
         std::uint16_t port;
         std::regex    controller_name_filter;
         std::string   mapping_file;
         bool          sensor_auto_toggle;
-        if (!parseProgramArgs(argc, argv, port, controller_name_filter, mapping_file, sensor_auto_toggle))
+        if (!parseProgramArgs(argc, argv, init_delay, port, controller_name_filter, mapping_file, sensor_auto_toggle))
         {
             return EXIT_FAILURE;
         }
+
+        if (!interuptable_sleep(init_delay))
+        {
+            return EXIT_SUCCESS;
+        }
+
+        BOOST_LOG_TRIVIAL(info) << "Initializing...";
 
         // Prepare general coroutine stuff
         constexpr int           no_concurrency{1};
         boost::asio::io_context io_context{no_concurrency};
         boost::asio::signal_set signals(io_context, SIGINT, SIGTERM);
-        signals.async_wait([&](auto, auto) { io_context.stop(); });
+        signals.async_wait([&io_context](auto, auto) { io_context.stop(); });
 
         // Prepare server stuff
         const auto server_id{server::generateServerId()};
